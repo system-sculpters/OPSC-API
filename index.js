199f3d92b7dbfa5bff8562144669bc0daa11c3b9
ST10215069-Nkosinathi-Ngozo <ST10215069@vcconnect.edu.za>
2024-10-13 01:46:25 +0200

    revert index.js

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
-const https = require('https')
-const dotenv = require('dotenv').config()
-const fs = require('fs')
-const app = require('./app')
-
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv').config();
+const cors = require('cors');
 const PORT = process.env.PORT || 3001
 
-// Define options for the HTTPS server using SSL/TLS certificates
-const options = {
-    key: fs.readFileSync('keys/privatekey.pem'),
-    cert: fs.readFileSync('keys/certificate.pem')
-}
-
-// Create an HTTPS server using the specified options and the Express app
-const server = https.createServer(options, app)
-
-// Start the server and have it listen on the specified port
-server.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-});
+app.use(cors());
+app.use(express.json());
  
+
+const authRoute = require('./routes/authRoutes');
+const userRoute = require('./routes/userRoutes');
+const transactionRoute = require('./routes/transactionRoutes');
+const categoryRoutes = require('./routes/categoryRoutes');
+const investmentRoutes = require('./routes/investmentRoutes');
+const stockRoutes = require('./routes/stockRoutes');
+const goalRoutes = require('./routes/goalRoutes');
+const analyticsRoutes = require('./routes/analyticsRoutes');
+const translationRoutes = require('./routes/translateRoutes');
+const notificationRoutes = require('./routes/notificationRouter');
+
+
+
+
+app.use('/api/auth/', authRoute);
+app.use('/api/user/', userRoute);
+app.use('/api/transaction/', transactionRoute);
+app.use('/api/category/', categoryRoutes);
+app.use('/api/investment/', investmentRoutes);
+app.use('/api/stocks/', stockRoutes);
+app.use('/api/goal/', goalRoutes);
+app.use('/api/analytics/', analyticsRoutes);
+app.use('/api/translation/', translationRoutes);
+app.use('/api/notification/', notificationRoutes);
+
+
+
+app.listen(PORT, () =>{
+    console.log(`backend server is running at localhost:${PORT}`);
+}); 
